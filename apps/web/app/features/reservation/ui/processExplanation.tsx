5aeb77218c03d634e20f1eedfc35e96ccda28d9c
nanikasi <nanikasi1024@gmail.com>
2024-09-19 09:46:24 +0000

    Remove human image

--- apps/web/app/features/reservation/ui/processExplanation.tsx
+++ apps/web/app/features/reservation/ui/processExplanation.tsx
@@ -5,7 +5,6 @@ type Props = {
 export const ProcessExplanation = ({ explanationText }: Props) => {
   return (
     <div className="w-full flex flex-col items-center text-center space-y-20">
-      <img src="/human.png" alt="Example" className="w-20 h-auto bg-gray-50" />
       <p className="mt-2 font-semibold">{explanationText}</p>
     </div>
   );
